2f05f432fa3ccacba43d95c998d5fcbf6aa92bca
csuhj <csuhj@users.noreply.github.com>
2021-11-15 19:57:55 +0000

    Broke the code up a bit and added some variation plus 2 more planets

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // https://threejsfundamentals.org/threejs/lessons/threejs-scenegraph.html
+// Also investigate https://dev.to/pahund/animating-camera-movement-in-three-js-17e9
 import * as THREE from './three.module.js';
+
+const SunRadius = 2;
 
 function main(canvas) {
   const renderer = new THREE.WebGLRenderer({canvas});
@@ -24,21 +27,22 @@ function main(canvas) {
 
   const objects = [];
 
-  const radius = 1;
-  const widthSegments = 6;
-  const heightSegments = 6;
-  const sphereGeometry = new THREE.SphereGeometry(
-      radius, widthSegments, heightSegments);
-
   const solarSystem = new THREE.Object3D();
   scene.add(solarSystem);
 
-  const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
-  const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-  sunMesh.scale.set(5, 5, 5);
-  solarSystem.add(sunMesh);
+  solarSystem.add(createSun());
 
-  solarSystem.add(createEarth(10, objects));
+  const mercury = createPlanet('mercury', 0.39, 0.3, objects);
+  const venus = createPlanet('venus', 0.72, 1, objects);
+  const earth = createPlanet('earth', 1, 1, objects);
+  addMoon(earth, 1, objects);
+
+  solarSystem.add(mercury);
+  solarSystem.add(venus);
+  solarSystem.add(earth);
+  
+  //Should really move the camera instead
+  solarSystem.rotation.x = Math.PI * 0.4;
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -61,7 +65,16 @@ function main(canvas) {
     }
 
     objects.forEach((obj) => {
-      obj.rotation.y = time;
+      let factor;
+      if (obj.name === 'mercury') {
+        factor = 1 / (88 / 365);
+      } else if (obj.name === 'venus') {
+        factor = 1 / (224 / 365);
+      } else {
+        factor = 1;
+      }
+
+      obj.rotation.y = (time * factor);
     });
 
     renderer.render(scene, camera);
@@ -72,48 +85,78 @@ function main(canvas) {
   requestAnimationFrame(render);
 }
 
-function createEarth(orbitRadius, objectsToRotate) {
+function createSun() {
+  const radius = 1;
+  const widthSegments = 50;
+  const heightSegments = 50;
+  const sphereGeometry = new THREE.SphereGeometry(
+      radius, widthSegments, heightSegments);
+
+  const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
+  const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+  sunMesh.scale.set(SunRadius, SunRadius, SunRadius);
+  return sunMesh;
+}
+
+function createPlanet(name, orbitRadiusInAU, planetRadiusInEarths, objectsToRotate) {
+  const orbitRadius = (orbitRadiusInAU * 10) + SunRadius;
+  const planetRadius = planetRadiusInEarths * 0.5;
+
+  const radius = planetRadius;
+  const widthSegments = 10;
+  const heightSegments = 10;
+  const sphereGeometry = new THREE.SphereGeometry(
+      radius, widthSegments, heightSegments);
+
+  const planetOrbit = new THREE.Object3D();
+  planetOrbit.name = name;
+  const planetOrbitCurve = new THREE.EllipseCurve(
+    0,  0,                    // ax, aY
+    orbitRadius, orbitRadius, // xRadius, yRadius
+    0,  2 * Math.PI,          // aStartAngle, aEndAngle
+  );
+  const planetOrbitGeometry = new THREE.BufferGeometry().setFromPoints( planetOrbitCurve.getPoints( 50 ) );
+  const planetOrbitMaterial = new THREE.LineBasicMaterial( { color: 0xFF0000 } );
+  const planetOrbitMesh = new THREE.LineLoop( planetOrbitGeometry, planetOrbitMaterial );
+  planetOrbitMesh.rotation.x = Math.PI / 2;
+
+  planetOrbit.add(planetOrbitMesh);
+  objectsToRotate.push(planetOrbit);
+
+  const planetGroup = new THREE.Object3D();
+  planetGroup.name = name;
+  planetGroup.position.x = orbitRadius;
+  planetOrbit.add(planetGroup);
+  objectsToRotate.push(planetGroup);
+
+  const planetMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
+  const planetMesh = new THREE.Mesh(sphereGeometry, planetMaterial);
+  planetGroup.add(planetMesh);
+  objectsToRotate.push(planetMesh);
+
+  return planetOrbit;
+}
+
+function addMoon(planetOrbit, orbitRadius, objectsToRotate) {
+    const planetGroup = planetOrbit.children.filter(c => c.name)[0];
+
     const radius = 1;
-    const widthSegments = 6;
-    const heightSegments = 6;
+    const widthSegments = 10;
+    const heightSegments = 10;
     const sphereGeometry = new THREE.SphereGeometry(
         radius, widthSegments, heightSegments);
 
-    const earthOrbit = new THREE.Object3D();
-    const earthOrbitCurve = new THREE.EllipseCurve(
-      0,  0,                    // ax, aY
-      orbitRadius, orbitRadius, // xRadius, yRadius
-      0,  2 * Math.PI,          // aStartAngle, aEndAngle
-    );
-    const earthOrbitGeometry = new THREE.BufferGeometry().setFromPoints( earthOrbitCurve.getPoints( 50 ) );
-    const earthOrbitMaterial = new THREE.LineBasicMaterial( { color: 0xFF0000 } );
-    const earthOrbitMesh = new THREE.LineLoop( earthOrbitGeometry, earthOrbitMaterial );
-    earthOrbitMesh.rotation.x = Math.PI / 2;
-  
-    earthOrbit.add(earthOrbitMesh);
-    objectsToRotate.push(earthOrbit);
-  
-    const earthGroup = new THREE.Object3D();
-    earthGroup.position.x = orbitRadius;
-    earthOrbit.add(earthGroup);
-    objectsToRotate.push(earthGroup);
-  
-    const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
-    const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
-    earthGroup.add(earthMesh);
-    objectsToRotate.push(earthMesh);
-  
     const moonOrbit = new THREE.Object3D();
-    moonOrbit.position.x = 2;
-    earthGroup.add(moonOrbit);
+    moonOrbit.position.x = orbitRadius;
+    planetGroup.add(moonOrbit);
   
     const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
     const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-    moonMesh.scale.set(.5, .5, .5);
+    moonMesh.scale.set(.25, .25, .25);
     moonOrbit.add(moonMesh);
     objectsToRotate.push(moonMesh);
 
-    return earthOrbit;
+    return moonOrbit;
 }
 
 export { main };
