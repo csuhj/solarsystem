ebfeb58c7eeda52c146d6c8b6d5d09ddd62ea724
csuhj <csuhj@users.noreply.github.com>
2021-11-21 13:24:13 +0000

    Refactored to make it easier to associate orbit periods with each planet/moon

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function main(canvas) {
   const near = 0.1;
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 500, 0);
+  camera.position.set(0, 50, 0);
   camera.up.set(0, 0, 1);
   camera.lookAt(0, 0, 0);
 
@@ -25,23 +25,23 @@ function main(canvas) {
     scene.add(light);
   }
 
-  const objects = [];
+  const objectsToRotate = [];
 
   const solarSystem = new THREE.Object3D();
   scene.add(solarSystem);
 
   solarSystem.add(createSun());
 
-  const mercury = createPlanet('mercury', 57.91, 2440, 0xcc5200, objects);
-  const venus = createPlanet('venus', 108.2, 6052, 0xffffb3,  objects);
-  const earth = createPlanet('earth',148, 6371, 0x66a3ff, objects);
-  addMoon(earth, 0.384, 1737, objects);
+  const mercury = createPlanet('mercury', 57.91, 2440, 88, 58.5, 0xcc5200, objectsToRotate);
+  const venus = createPlanet('venus', 108.2, 6052, 225, 116.6, 0xffffb3,  objectsToRotate);
+  const earth = createPlanet('earth',148, 6371, 365.25, 1, 0x66a3ff, objectsToRotate);
+  addMoon(earth, 0.384, 1737, 27.3, objectsToRotate);
 
-  const mars = createPlanet('mars', 239.67, 3390, 0xff471a, objects);
-  const jupiter = createPlanet('jupiter', 778, 69911, 0xff9966, objects);
-  const saturn = createPlanet('saturn', 1434, 58232, 0xcca300, objects);
-  const uranus = createPlanet('uranus', 2871, 25362, 0x99ccff, objects);
-  const neptune = createPlanet('neptune', 4495, 24262, 0x0066cc, objects);
+  const mars = createPlanet('mars', 239.67, 3390, 687, 1, 0xff471a, objectsToRotate);
+  const jupiter = createPlanet('jupiter', 778, 69911, 4333, 0.3, 0xff9966, objectsToRotate);
+  const saturn = createPlanet('saturn', 1434, 58232, 10759, 0.4, 0xcca300, objectsToRotate);
+  const uranus = createPlanet('uranus', 2871, 25362, 30688, 0.7, 0x99ccff, objectsToRotate);
+  const neptune = createPlanet('neptune', 4495, 24262, 60195, 0.7, 0x0066cc, objectsToRotate);
 
   solarSystem.add(mercury);
   solarSystem.add(venus);
@@ -53,6 +53,7 @@ function main(canvas) {
   solarSystem.add(neptune);
   
   //Should really move the camera instead
+  solarSystem.rotation.y = Math.PI * 1.6;
   solarSystem.rotation.x = Math.PI * 0.4;
 
   function resizeRendererToDisplaySize(renderer) {
@@ -75,27 +76,9 @@ function main(canvas) {
       camera.updateProjectionMatrix();
     }
 
-    objects.forEach((obj) => {
-      let factor;
-      if (obj.name === 'mercury') {
-        factor = 1 / (88 / 365);
-      } else if (obj.name === 'venus') {
-        factor = 1 / (225 / 365);
-      } else if (obj.name === 'mars') {
-        factor = 1 / (687 / 365);
-      } else if (obj.name === 'jupiter') {
-        factor = 1 / (4333 / 365);
-      } else if (obj.name === 'saturn') {
-        factor = 1 / (10759 / 365);
-      } else if (obj.name === 'uranus') {
-        factor = 1 / (30688 / 365);
-      } else if (obj.name === 'neptune') {
-        factor = 1 / (60195 / 365);
-      } else {
-        factor = 1;
-      }
-
-      obj.rotation.y = (time * factor);
+    objectsToRotate.forEach((objToRotate) => {
+      let factor = 1 / (objToRotate.periodInEarthDays / 365.25);
+      objToRotate.obj.rotation.y = (time * factor);
     });
 
     renderer.render(scene, camera);
@@ -119,7 +102,7 @@ function createSun() {
   return sunMesh;
 }
 
-function createPlanet(name, orbitRadiusInMKm, planetRadiusInKm, colour, objectsToRotate) {
+function createPlanet(name, orbitRadiusInMKm, planetRadiusInKm, lengthOfYearInEarthDays, lengthOfDayInEarthDays, colour, objectsToRotate) {
   const orbitRadius = ((orbitRadiusInMKm / 149) * 10) + SunRadius;
   const planetRadius = (planetRadiusInKm / 6371) * 0.5;
 
@@ -142,40 +125,38 @@ function createPlanet(name, orbitRadiusInMKm, planetRadiusInKm, colour, objectsT
   planetOrbitMesh.rotation.x = Math.PI / 2;
 
   planetOrbit.add(planetOrbitMesh);
-  objectsToRotate.push(planetOrbit);
+  objectsToRotate.push({ obj: planetOrbit, periodInEarthDays: lengthOfYearInEarthDays });
 
   const planetGroup = new THREE.Object3D();
   planetGroup.name = name;
   planetGroup.position.x = orbitRadius;
   planetOrbit.add(planetGroup);
-  objectsToRotate.push(planetGroup);
 
   const planetMaterial = new THREE.MeshPhongMaterial({color: colour, emissive: 0x444444});
   const planetMesh = new THREE.Mesh(sphereGeometry, planetMaterial);
   planetGroup.add(planetMesh);
-  objectsToRotate.push(planetMesh);
+  objectsToRotate.push({ obj: planetMesh, periodInEarthDays: lengthOfDayInEarthDays });
 
   return planetOrbit;
 }
 
-function addMoon(planetOrbit, orbitRadius, moonRadiusInKm, objectsToRotate) {
+function addMoon(planetOrbit, orbitRadius, moonRadiusInKm, lengthOfMonthInEarthDays, objectsToRotate) {
     const planetGroup = planetOrbit.children.filter(c => c.name)[0];
 
-    const radius = (moonRadiusInKm / 6371);
+    const radius = (moonRadiusInKm / 6371) * 0.5;
     const widthSegments = 10;
     const heightSegments = 10;
     const sphereGeometry = new THREE.SphereGeometry(
         radius, widthSegments, heightSegments);
 
     const moonOrbit = new THREE.Object3D();
-    moonOrbit.position.x = 1; //orbitRadius; - needs to be orbitRadius plus planet's radius
     planetGroup.add(moonOrbit);
   
     const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
     const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-    moonMesh.scale.set(.25, .25, .25);
+    moonMesh.position.x = 1; //orbitRadius; - needs to be orbitRadius plus planet's radius
     moonOrbit.add(moonMesh);
-    objectsToRotate.push(moonMesh);
+    objectsToRotate.push({ obj: moonOrbit, periodInEarthDays: lengthOfMonthInEarthDays });
 
     return moonOrbit;
 }
